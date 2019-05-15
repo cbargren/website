b0e46720c14dab88e0ff2422d1e1812b038cc43d
Chris Bargren <cbargren@gmail.com>
2019-05-15 11:51:43 -0700

    Updating all the things

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,4 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { addBabelPlugins, override } = require('customize-cra');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function override(config, env) {
-  return injectBabelPlugin('styled-components', config);
-};
+module.exports = override(addBabelPlugins('styled-components'));
